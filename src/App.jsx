ffe8bc3d6bf698409c47c2ae226d13d9ab763546
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-02 16:55:28 +0200

    HTML setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { FiTwitter } from "react-icons/fi";
 
 function App() {
-
   return (
-    <div className="App">
-      Hej
+    <div id="quote-box">
+      <h1 id="text">Random quote goes here</h1>
+      <span id="author"> - Aurhor goes here</span>
+      <section>
+        <a id="tweet-qutoe" href="twitter.com/intent/tweet" target="_blank">
+          <FiTwitter>Twitter icon</FiTwitter>
+        </a>
+        <Button id="new-quote" variant="dark">
+          New quote
+        </Button>
+      </section>
     </div>
   );
 }
