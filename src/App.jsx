a197b7f01ae80dd4133c6f6fcf561e030a78feba
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-04 01:11:30 +0200

    Quotes up and runing

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,44 @@ import React, { useEffect } from "react";
 import { Button, Container } from "react-bootstrap";
 import { FiTwitter } from "react-icons/fi";
 import { useDispatch, useSelector } from "react-redux";
-// import { getQuote } from "./Redux/Actions/apiActions";
+import { getQuote } from "./Redux/Actions/apiActions";
 
 function App() {
-  // const getQuotes = useSelector((state) => state.getQuotes.quote);
+  let quote = useSelector((state) => state.getQuote.quote);
+  console.log(quote + " data should go here");
+  const { id, content, originator } = quote;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // dispatch(getQuote());
+    dispatch(getQuote());
   }, []);
 
-  const renderQuotes = () => {
-    // if (!getQuotes.length) return <div>Loading quote...</div>;
-    // return getQuotes.map((quote) => {
-    //   const { id, content, originator } = quote;
-    //   return (
-    //     <section key={id}>
-    //       {Object.keys(quote).length === 0 ? (
-    //         <div>...Loading</div>
-    //       ) : (
-    //         <Container>
-    //           <div id="quote-box">
-    //             <h1 id="text">{content}</h1>
-    //             <span id="author"> - {originator}</span>
-    //             <section>
-    //               <a
-    //                 id="tweet-qutoe"
-    //                 href="twitter.com/intent/tweet"
-    //                 target="_blank"
-    //               >
-    //                 <FiTwitter>Twitter icon</FiTwitter>
-    //               </a>
-    //               <Button id="new-quote" variant="dark">
-    //                 New quote
-    //               </Button>
-    //             </section>
-    //           </div>
-    //         </Container>
-    //       )}
-    //       ;
-    //     </section>
-    //   );
-    // });
-  };
-
-  return <><div>HELLO</div></>;
+  return (
+    <>
+      {Object.keys(quote).length === 0 ? (
+        <div>...Loading</div>
+      ) : (
+        <Container key={id}>
+          <div id="quote-box">
+            <h1 id="text">{content}</h1>
+            <span id="author"> - {originator.name}</span>
+            <section>
+              <a
+                id="tweet-qutoe"
+                href="twitter.com/intent/tweet"
+                target="_blank"
+              >
+                <FiTwitter>Twitter icon</FiTwitter>
+              </a>
+              <Button id="new-quote" variant="dark">
+                New quote
+              </Button>
+            </section>
+          </div>
+        </Container>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/Redux/Actions/apiActions.js
+++ src/Redux/Actions/apiActions.js
@@ -1,29 +1,20 @@
 import axios from "axios";
 import { ActionType } from "../constans/action-type";
 
-const options = {
-    method: 'GET',
-    url: 'https://quotes15.p.rapidapi.com/quotes/random/',
-    params: { language_code: 'en' },
-    headers: {
-        'X-RapidAPI-Host': 'quotes15.p.rapidapi.com',
-        'X-RapidAPI-Key': process.env.VITE_SOME_KEY,
-    }
-};
-
-axios.request(options).then(function (response) {
-    console.log(response.data);
-}).catch(function (error) {
-    console.error(error);
-});
-
-console.log(process.env.VITE_SOME_KEY)
+var mykey = config.VITE_SOME_KEY;
 
 export const getQuote = () => async (dispatch) => {
-    const res = await axios.get('https://quotes15.p.rapidapi.com/quotes/random/');
+    const res = await axios.get('https://quotes15.p.rapidapi.com/quotes/random/?language_code=en', {
+        headers: {
+            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com',
+            'X-RapidAPI-Key': mykey,
+        }
+    });
     dispatch({
         type: ActionType.GET_QUOTE,
         quote: res.data,
     });
     console.log(res.data, " res getQuote");
 }
+
+console.log(mykey + " key")
