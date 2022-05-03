97f2284d52c983d6e91699fa84ee98510d6cbe71
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-03 22:21:41 +0200

    Build of component render for quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { Button, Container } from "react-bootstrap";
 import { FiTwitter } from "react-icons/fi";
+import { useDispatch, useSelector } from "react-redux";
+import { getQuote } from "./Redux/Actions/apiActions";
 
 function App() {
-  return (
-    <div id="quote-box">
-      <h1 id="text">Random quote goes here</h1>
-      <span id="author"> - Aurhor goes here</span>
-      <section>
-        <a id="tweet-qutoe" href="twitter.com/intent/tweet" target="_blank">
-          <FiTwitter>Twitter icon</FiTwitter>
-        </a>
-        <Button id="new-quote" variant="dark">
-          New quote
-        </Button>
-      </section>
-    </div>
-  );
+  const getQuotes = useSelector((state) => state.getQuotes.quote);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getQuote());
+  }, []);
+
+  const renderQuotes = () => {
+    if (!getQuotes.length) return <div>Loading quote...</div>;
+    return getQuotes.map((quote) => {
+      const { id, content, originator } = quote;
+      return (
+        <section key={id}>
+          {Object.keys(quote).length === 0 ? (
+            <div>...Loading</div>
+          ) : (
+            <Container>
+              <div id="quote-box">
+                <h1 id="text">{content}</h1>
+                <span id="author"> - {originator}</span>
+                <section>
+                  <a
+                    id="tweet-qutoe"
+                    href="twitter.com/intent/tweet"
+                    target="_blank"
+                  >
+                    <FiTwitter>Twitter icon</FiTwitter>
+                  </a>
+                  <Button id="new-quote" variant="dark">
+                    New quote
+                  </Button>
+                </section>
+              </div>
+            </Container>
+          )}
+          ;
+        </section>
+      );
+    });
+  };
+
+  return <>{renderQuotes}</>;
 }
 
 export default App;
