68090e795bdca2b5655f58cb6d53769bdbc97d14
Linda Andersson <linda.29.andersson@gmail.com>
2022-05-04 16:39:43 +0200

    Testing new url

--- src/Redux/Actions/apiActions.js
+++ src/Redux/Actions/apiActions.js
@@ -4,7 +4,7 @@ import { ActionType } from "../constans/action-type";
 export const getQuote = () => async (dispatch) => {
     const res = await axios.request({
         method: 'GET',
-        url: import.meta.env.VITE_API_URL,
+        url: `https://${import.meta.env.VITE_API_URL}`,
         headers: {
             'X-RapidAPI-Host': import.meta.env.VITE_API_HOST,
             'X-RapidAPI-Key': import.meta.env.VITE_API_KEY,
