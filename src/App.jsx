e9b0eeac5d2971df230046eff25facf7bf582713
Vincent Sjögren <vincentsjogren@gmail.com>
2022-05-04 17:29:49 +0200

    added cleanup function to useEffect in App by updating getQuote function

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,61 @@
-import React, { useEffect } from "react";
-import { Button, Container } from "react-bootstrap";
-import { FiTwitter } from "react-icons/fi";
-import { useDispatch, useSelector } from "react-redux";
-import { getQuote } from "./Redux/Actions/apiActions";
+import React, { useEffect } from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { FiTwitter } from 'react-icons/fi';
+import { useDispatch, useSelector } from 'react-redux';
+import { getQuote } from './Redux/Actions/apiActions';
 
 function App() {
-  let quote = useSelector((state) => state.getQuote.quote);
-  const { id, content, originator } = quote;
-  const dispatch = useDispatch();
+	let quote = useSelector((state) => state.getQuote.quote);
+	const { id, content, originator } = quote;
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getQuote());
-  }, []);
+	useEffect(() => {
+		const controller = new AbortController();
+		dispatch(getQuote(controller));
+		return () => controller.abort();
+	}, []);
 
-  const handleNewQuote = () => {
-    dispatch(getQuote());
-  };
+	const handleNewQuote = () => {
+		dispatch(getQuote());
+	};
 
-  return (
-    <>
-      {Object.keys(quote).length === 0 ? (
-        <div>...Loading</div>
-      ) : (
-        <Container key={id} style={{ maxWidth: 1000 }}>
-          <div id="quote-box">
-            <h1 id="text">"{content}"</h1>
-            <span id="author"> - {originator.name}</span>
-            <section>
-              <a
-                id="tweet-quote"
-                className="twitter-share-button"
-                href={`https://twitter.com/intent/tweet?text=${content} -${originator.name}`}
-                target="_blank"
-              >
-                <FiTwitter>Tweet</FiTwitter>
-              </a>
-              <Button
-                id="new-quote"
-                variant="dark"
-                onClick={() => handleNewQuote()}
-              >
-                New quote
-              </Button>
-            </section>
-            <span>
-              Powerd by:{" "}
-              <a href="https://quotepark.com/" target="_blank">
-                https://quotepark.com/
-              </a>
-            </span>
-          </div>
-        </Container>
-      )}
-    </>
-  );
+	return (
+		<>
+			{Object.keys(quote).length === 0 ? (
+				<div>...Loading</div>
+			) : (
+				<Container key={id} style={{ maxWidth: 1000 }}>
+					<div id="quote-box">
+						<h1 id="text">"{content}"</h1>
+						<span id="author"> - {originator.name}</span>
+						<section>
+							<a
+								id="tweet-quote"
+								className="twitter-share-button"
+								href={`https://twitter.com/intent/tweet?text=${content} -${originator.name}`}
+								target="_blank"
+							>
+								<FiTwitter>Tweet</FiTwitter>
+							</a>
+							<Button
+								id="new-quote"
+								variant="dark"
+								onClick={() => handleNewQuote()}
+							>
+								New quote
+							</Button>
+						</section>
+						<span>
+							Powerd by:{' '}
+							<a href="https://quotepark.com/" target="_blank">
+								https://quotepark.com/
+							</a>
+						</span>
+					</div>
+				</Container>
+			)}
+		</>
+	);
 }
 
 export default App;
